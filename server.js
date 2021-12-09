23f58b8f63c0eb3f6532a049e72258d7bbaf6e75
Nuno Jerónimo <43753007+ioAcademy-NunoJeronimo@users.noreply.github.com>
2021-12-09 13:18:20 +0200

    post1

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const path = require("path");
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
 
 server.listen(port);
